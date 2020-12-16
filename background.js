63fcfaa29e1361a0891872afad5bb510d6928220
nimishagarwal76 <nimishagarwal76@gmail.com>
2020-12-16 17:00:34 +0530

    Add setup for header transfer

--- background.js
+++ background.js
@@ -1,19 +1,21 @@
 var tabLink = {};
+var tabRef = {};
 
 chrome.webRequest.onBeforeRequest.addListener((details) => {
-    let { tabId, url, method } = details;
+    let { tabId, url, method, initiator } = details;
 
     if (details.type == "main_frame") {
         let redirectUrl = chrome.extension.getURL('video.html') + "#" + details.url;
         return { redirectUrl }
     }
 
-    if (method == "GET") {
+    if (method == "GET" && initiator.startsWith("http")) {
         if (tabLink[tabId]) {
             tabLink[tabId].push(url);
         } else {
             tabLink[tabId] = [url];
         }
+        // tabRef[url] = initiator;
 
         if (tabLink[tabId].length == 1) {
             chrome.browserAction.setIcon({
@@ -25,12 +27,33 @@ chrome.webRequest.onBeforeRequest.addListener((details) => {
 
 }, { urls: ["*://*/*.m3u8*"] }, ["blocking"])
 
+chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+    
+    let {tabId} = details;
+    // console.log("da");
+    let { url } = details,
+        refIndex = details.requestHeaders.findIndex(header => header.name === "Referer");
+
+    if(refIndex === -1 && tabRef[tabId]) {
+
+            details.requestHeaders.push({ name: "Referer", value: tabRef[tabId] })
+    }
+    // let initiator = tabRef[url];
+    // if (refIndex === -1 && initiator) {
+    //     if(!initiator.endsWith("/")) initiator += "/";
+    // }
+    return { requestHeaders: details.requestHeaders };
+}, { urls: ["*://*/*.m3u8*", "*://*/*.ts*"] }, ['requestHeaders', 'blocking', 'extraHeaders']);
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    console.log(message);
     let { tabId } = message;
     let data;
     if (!tabLink[tabId]) data = [];
     else data = tabLink[tabId];
+    if(message.msg) {
+        tabRef[message.msg] = message.url;
+    }
 
     sendResponse({ data })
 });
--- popup.js
+++ popup.js
@@ -1,33 +1,54 @@
-var linkList = document.getElementById("links");
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    let pageUrl = tabs[0].url;
+    
 
-var getLinkNode = (link) => {
-    return `
+    var linkList = document.getElementById("links");
+
+    var getLinkNode = (link) => {
+        return `
     <div class="linkbox">
         <span class="link">${link}</span>
         <div class="icongroup">
-            <a href="${link}" target="_blank"><img src="icons/newtab.png" class="icon" id="newtab"/></a>
+            <img src="icons/newtab.png" class="icon" vidurl="${link}" id="newtab"/>
         </div>
     </div>
     `
-}
-
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    let tabId = tabs[0].id;
-    chrome.runtime.sendMessage({ tabId }, (response) => {
-        let { data } = response;
-        if (data.length == 0) {
-            linkList.innerHTML = "<center>No streams found</center>"
-        } else {
-            data.forEach(url => {
-                linkList.innerHTML += getLinkNode(url);
-            })
-        }
-    });
-});
-
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        linkList.innerHTML += JSON.stringify(request);
-
     }
-);
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let tabId = tabs[0].id;
+        chrome.runtime.sendMessage({ tabId }, (response) => {
+            let { data } = response;
+            if (data.length == 0) {
+                linkList.innerHTML = "<center>No streams found</center>"
+            } else {
+                data.forEach(url => {
+                    linkList.innerHTML += getLinkNode(url);
+                })
+            }
+        });
+    });
+
+    chrome.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+            linkList.innerHTML += JSON.stringify(request);
+
+        }
+    );
+
+    linkList.addEventListener("click", (e) => {
+        let url = e.target.getAttribute("vidurl");
+        /**
+         * HACK : need to pass tabId to background.js
+         * selected = false helps that to happen else popup.js will close as focus changes
+         * and information regarding referer won't be passed to background
+         */
+        chrome.tabs.create({ url, selected: false }, (tab) => {
+            chrome.runtime.sendMessage({
+                msg: tab.id,
+                url: pageUrl
+            })
+        })
+    })
+
+});
